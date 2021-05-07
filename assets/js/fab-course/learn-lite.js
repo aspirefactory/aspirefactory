38ec07e271fb0c0f791049a63b8603ff5e4b0645
gopalakrishnan-r <gopalakrishnars94@gmail.com>
2021-05-07 16:18:51 +0530

    dummy content added in fab-course

---
+++ assets/js/fab-course/learn-lite.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $.getJSON("/offering/fab-course/join-course/learn-lite/learn-lite.json",
+        function(data) {
+            var content = '';
+            $.each(data, function(key, value) {
+                content += '<div class="row">'
+                content += '<div class="col-lg-6">'
+                content += '<iframe width="420" height="315" src="https://www.youtube.com/embed/' + value.url + '"></iframe>'
+                content += '</div>'
+                content += '<div class="col-lg-6">'
+                content += '<h4>' + value.title + '</h4>'
+                content += '<hr class="divider ml-0">'
+                content += '<p class="text-muted">' + value.desc + '</h6>'
+                content += '</div>'
+                content += '</div>'
+                content += '<hr>'
+
+
+
+            });
+            $('#course-container').append(content);
+        });
+});
